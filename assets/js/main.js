c03f80d0b592b3967ffcef173d5404340037b56c
Dat <dathn.6995@gmail.com>
2021-06-21 14:35:51 +0900

    Footer Scroll Highlight Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,10 +105,10 @@ modalCloses.forEach((modalClose) => {
 
 // PORTFOLIO SWIPER
 
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
-    
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -122,3 +122,46 @@ let swiper = new Swiper(".portfolio__container", {
     // mousewheel: true,
     // keyboard: true,
 });
+
+// TESTIMONIAL SWIPER
+
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+
+    // mousewheel: true,
+    // keyboard: true,
+});
+
+// SCROLL SECTIONS ACTIVE LINK
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
